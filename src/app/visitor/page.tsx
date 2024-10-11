f64d529107c75b000e1cef73af15fc2e6c2ab568
İnanç CAN <ge82ced@mytum.de>
2024-10-11 16:30:29 +0200

    Refactor auth process and component in VoteBox

--- src/app/visitor/page.tsx
+++ src/app/visitor/page.tsx
@@ -1,8 +1,11 @@
+"use client";
+import { useEffect, useState } from "react";
 import SearchBar from "@/components/SearchBar";
 import VoteTable from "@/components/Vote/VoteTable";
 import SignInButton from "@/components/SignInButton";
 import { supabase } from "@/lib/supabase";
-export default async function Search({
+
+export default function Search({
   searchParams,
 }: {
   searchParams?: {
@@ -12,25 +15,27 @@ export default async function Search({
 }) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
-  // Check if the user is already authenticated
-  const {
-    data: { user },
-    error: userError,
-  } = await supabase.auth.getUser();
 
-  // If the user is not authenticated, sign in anonymously
-  if (!user) {
-    const { data: authData, error: authError } =
-      await supabase.auth.signInAnonymously();
+  const [loading, setLoading] = useState(true);
 
-    if (authError) {
-      console.error("Error during anonymous sign-in:", authError.message);
-    } else {
-      console.log("Anonymous user signed in:", authData);
-    }
-  } else {
-    console.log("User is already authenticated:", user);
+  useEffect(() => {
+    const signInAnonymously = async () => {
+      const { error } = await supabase.auth.signInAnonymously();
+      if (error) {
+        console.error("Error during anonymous sign-in:", error.message);
+      } else {
+        console.log("User signed in anonymously");
+      }
+      setLoading(false);
+    };
+
+    signInAnonymously();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
   }
+
   return (
     <div className="relative min-h-screen">
       <div className="mx-8 my-4">
