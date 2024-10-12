9f32cf1f228e96cb5918588079bcd37dd87865d8
İnanç CAN <ge82ced@mytum.de>
2024-10-12 16:53:54 +0200

    Refactor visitor page component to handle anonymous sign-in and loading state

--- src/app/visitor/page.tsx
+++ src/app/visitor/page.tsx
@@ -1,7 +1,10 @@
+"use client"
 import SearchBar from "@/components/SearchBar";
 import VoteTable from "@/components/Vote/VoteTable";
 import SignInButton from "@/components/SignInButton";
 import { supabase } from "@/lib/supabase";
+import { useEffect, useState } from "react";
+
 export default async function Search({
   searchParams,
 }: {
@@ -13,14 +16,28 @@ export default async function Search({
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
   const { data: authData, error: authError } =
-    await supabase.auth.signInAnonymously();
+  await supabase.auth.signInAnonymously();
 
-  if (authError) {
-    console.error("Error during anonymous sign-in:", authError.message);
-  } else {
-    console.log(authData);
-  }
-
+const [loading, setLoading] = useState(true);
+useEffect(() => {
+  const signInAnonymously = async () => {
+    const { error } = await supabase.auth.signInAnonymously();
+    if (error) {
+      console.error("Error during anonymous sign-in:", error.message);
+    } else {
+      console.log("User signed in anonymously");
+    }
+    setLoading(false);
+  };
+  signInAnonymously();
+}, []);
+if (loading) {
+  return <div>Loading...</div>;
+if (authError) {
+  console.error("Error during anonymous sign-in:", authError.message);
+} else {
+  console.log(authData);
+}
   return (
     <div className="relative min-h-screen">
       <div className="mx-8 my-4">
