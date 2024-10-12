c5a13baf9046765aef282f77f152287c636e992d
İnanç CAN <ge82ced@mytum.de>
2024-10-12 16:59:57 +0200

    Refactor visitor page component to handle anonymous sign-in and loading state

--- src/app/visitor/page.tsx
+++ src/app/visitor/page.tsx
@@ -1,11 +1,11 @@
-"use client"
+"use client";
+import { useEffect, useState } from "react";
 import SearchBar from "@/components/SearchBar";
 import VoteTable from "@/components/Vote/VoteTable";
 import SignInButton from "@/components/SignInButton";
 import { supabase } from "@/lib/supabase";
-import { useEffect, useState } from "react";
 
-export default async function Search({
+export default function Search({
   searchParams,
 }: {
   searchParams?: {
@@ -15,33 +15,31 @@ export default async function Search({
 }) {
   const query = searchParams?.query || "";
   const currentPage = Number(searchParams?.page) || 1;
-  const { data: authData, error: authError } =
-  await supabase.auth.signInAnonymously();
 
-const [loading, setLoading] = useState(true);
-useEffect(() => {
-  const signInAnonymously = async () => {
-    const { error } = await supabase.auth.signInAnonymously();
-    if (error) {
-      console.error("Error during anonymous sign-in:", error.message);
-    } else {
-      console.log("User signed in anonymously");
-    }
-    setLoading(false);
-  };
-  signInAnonymously();
-}, []);
-if (loading) {
-  return <div>Loading...</div>;
-if (authError) {
-  console.error("Error during anonymous sign-in:", authError.message);
-} else {
-  console.log(authData);
-}
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const signInAnonymously = async () => {
+      const { error } = await supabase.auth.signInAnonymously();
+      if (error) {
+        console.error("Error during anonymous sign-in:", error.message);
+      } else {
+        console.log("User signed in anonymously");
+      }
+      setLoading(false);
+    };
+
+    signInAnonymously();
+  }, []);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
   return (
     <div className="relative min-h-screen">
       <div className="mx-8 my-4">
-        <SearchBar placeholder="Search" />
+        <SearchBar placeholder="Search a song" />
         <VoteTable query={query} currentPage={currentPage} />
       </div>
       <SignInButton />
